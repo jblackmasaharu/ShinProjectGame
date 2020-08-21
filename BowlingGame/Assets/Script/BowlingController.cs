0828da27e226b57f59bb053b5a9adf3764fc3fd9
Masaharu1029 <taku.roh.kabu@gmail.com>
2020-08-22 04:11:53 +0900

    ボーリングゲーム製作

---
+++ BowlingGame/Assets/Script/BowlingController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingController : MonoBehaviour
+{
+    public GameObject Ball;
+    Rigidbody rbBall;
+    public float power = 10f;
+    private int shotCount;
+    const int shotCountMax = 3;
+    public GameObject ballNumbertext;
+
+    CharacterController ccPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbBall = Ball.GetComponent<Rigidbody>();
+        ccPlayer = transform.root.gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bowlingThrow();
+    }
+
+    public void bowlingThrow()
+    {
+        if (Input.GetMouseButtonDown(0))    // マウスを左クリックした時
+        {
+            Vector3 arm = transform.position;
+            arm.y -= -1;
+
+            shotCount++;
+
+            if (shotCount <= shotCountMax)
+            {
+                rbBall = Instantiate(Ball, arm, transform.rotation).GetComponent<Rigidbody>(); // 玉を生成
+                rbBall.AddForce(ccPlayer.velocity * power + transform.forward, ForceMode.Impulse); // プレイヤーの前方に力を加える
+            }
+
+
+        }
+
+    }
+
+}
---
+++ BowlingGame/Assets/Script/GameScript3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript3 : MonoBehaviour
+{
+    public Text text;
+    public Image textGround;
+    GameObject[] pins;
+    public float q = 0.01f;
+    public GameObject gameCleartext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pins = GameObject.FindGameObjectsWithTag("Pin"); //「Pin」タグのついたオブジェクトを探して、全て配列pinsにいれる
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (text.text == "")
+            {
+                text.text = CountStandingPins().ToString();
+            }
+            else
+            {
+                text.text = "";
+            }
+
+        }
+        if (pins.Length == 0)
+        {
+            gameCleartext.SetActive(true);
+        }
+    }
+
+    int CountStandingPins()
+    {
+        pins = GameObject.FindGameObjectsWithTag("Pin"); //「Pin」タグのついたオブジェクトを探して、全て配列pinsにいれる
+
+        if (pins.Length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            int n = 0;
+
+            foreach (GameObject pin in pins)
+            {
+                if (pin.transform.rotation.x * pin.transform.rotation.x < q && pin.transform.rotation.z * pin.transform.rotation.z < q)
+                {
+                    n++;
+                }
+            }
+            return n; // 立っているピンの個数を返す
+        }
+    }
+}
